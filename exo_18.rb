30d492945741d31a0c3aa753203d0be883fe6236
Baptoow <baptistebarbera@gmail.com>
2020-09-29 18:59:07 +0200

    exo 18

--- exo_18.rb
+++ exo_18.rb
@@ -1,8 +1,15 @@
 n = 1
-mailListe = Array.new()
-while (n<=50)
-    email = "jean.dupont.#{n}@email.fr"
-  mailListe.push(email)
-  n = n + 1
-end
-puts mailListe
+email_list = Array.new() 
+
+	while (n <= 50)
+		if (n < 10)
+			email = "jean.dupont.0#{n}@email.fr" 
+		else
+			email = "jean.dupont.#{n}@email.fr"
+		end
+
+		email_list.push(email)
+	
+		n = n +1
+	end
+	puts email_list
